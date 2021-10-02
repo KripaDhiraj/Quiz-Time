c33937a42c5713b0f21618833c1256ba16fdbd00
KripaDhiraj <86722435+KripaDhiraj@users.noreply.github.com>
2021-10-02 15:42:32 +0530

    Add files via upload

---
+++ js/Question.js
@@ -0,0 +1,53 @@
+class Question {
+
+  constructor() {
+    this.title = createElement('h1')
+    this.input1 = createInput("Enter Your Name Here....");
+    this
+   //Create a input box to enter the number
+    this.input=createInput('5')
+
+
+    this.button = createButton('Submit');
+    //Create elements for One question (h3) and 4 options (h4)
+     createElement ('h1'( )) 
+      
+
+
+
+    this.message = createElement("h2")
+  }
+
+  hide(){
+     this.title.hide();
+    this.input1.hide();
+    this.button.hide();
+    this.input2.hide();
+    this.message.hide();
+
+
+
+
+  }
+
+  display(){
+    this.title.html("MyQuiz Game");
+    this.title.position(350, 0);
+
+
+    //Create html() and position() for each question, input and answers.
+
+
+
+
+    this.input1.position(150, 230);
+    this.button.position(290, 300);
+
+    // Add a mousepressed action when the button is clicked Display the this.message
+    this.button.mousePressed(()=>{
+
+    })
+
+
+  }
+}
